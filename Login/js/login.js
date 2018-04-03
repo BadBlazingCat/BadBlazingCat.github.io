52094022e4cfc691afdf3ab806c8c21bd5866303
BadBlazingCat <anuoluwapo.soneye1@gmail.com>
2018-04-02 22:05:22 -0700

    For Login

---
+++ Login/js/login.js
@@ -0,0 +1,9 @@
+ const txtEmail = document.getElementById("txtEmail");
+ const txtPassword = document.getElementById("txtPassword");
+ const btnSignIn = document.getElementById("btnSignIn");
+ const btnSignUp = document.getElementById("btnSignUp");
+
+ //Add SignIn event
+ btnSignIn.addEventListener("dblclick", function() {
+   window.alert("wee");
+ });
